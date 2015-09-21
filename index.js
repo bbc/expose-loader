97aa5fcda7101a352208354e51b419487eacc721
Ben Lambert <benjamin.lambert@bbc.co.uk>
2015-09-21 12:37:28 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 module.exports = function() {};
 module.exports.pitch = function(remainingRequest) {
-	return this.query.substr(1) + ' = require(' + JSON.stringify('-!' + remainingRequest) + ');';
+    return this.query.substr(1) + ' = require(' + JSON.stringify('-!' + remainingRequest) + '); ' + this.query.substr(1) + '.dependencies = ' + JSON.stringify(global.moduleDependencies);
 };
+
